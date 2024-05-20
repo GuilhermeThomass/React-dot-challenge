dd497eabb871e249970ee4f242073a5736b8ee12
Guilherme Thomas da Costa <guilhermethomas.dev@gmail.com>
2024-05-20 20:03:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Global from './style/global';
+import { Section,Dot, Button, Container } from './style/appStyle';
 
 function App() {
+  const [list,setList] = useState([]);
+  const [undid,setUndid] = useState([]);
+  
+  const handleClick = (e) =>{
+    const newDot = {
+      x: e.clientX,
+      y: e.clientY
+    }
+    setList((prev) => [...prev, newDot]);
+  }
+
+  const handleUndo = (e) =>{
+    e.stopPropagation();
+
+    if(list.length === 0){
+      return;
+    }
+
+    const lastItem = list[list.length-1];
+    setUndid((prev)=>[...prev, lastItem]);
+
+    setList((prev) => {
+      const newArray = [...prev].slice(0,-1);
+      return newArray
+    });
+  }
+  const handleRedo = (e) =>{
+    e.stopPropagation();
+
+    if(undid.length === 0){
+      return;
+    }
+
+    const lastDot = undid[undid.length-1];
+
+    setUndid((prev) => {
+      const newArray = [...prev].slice(0,-1);
+      return newArray
+    });
+    setList((prev) => [...prev, lastDot]);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Section onClick={handleClick}>
+      <Container>
+        <Button onClick={handleUndo}>Undo</Button>
+        <Button onClick={handleRedo}>Redo</Button>
+      </Container>
+      <Global/>
+      {list.map((item) => (
+        <Dot style={{left: item.x - 10, top: item.y - 10 }}/>
+      ))}
+    </Section>
+
   );
 }
 
